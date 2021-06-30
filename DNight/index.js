6f43987e38d909c8371cf6e25452677f69263789
pong777 <gksmf0760@gmail.com>
2021-06-30 19:02:00 +0900

    연동테스트

--- DNight/index.js
+++ DNight/index.js
@@ -1,21 +1,4 @@
 (function () {
   
-	var accItem = document.getElementsByClassName('accordionItem');
-    var accHD = document.getElementsByClassName('accordionItemHead');
-    for (i = 0; i < accHD.length; i++) {
-        accHD[i].addEventListener('click', toggleItem, false);
-    }
-    function toggleItem() {
-        var itemClass = this.parentNode.className;
-        for (i = 0; i < accItem.length; i++) {  
-            accItem[i].className = 'accordionItem close';
-        }
-        if (itemClass == 'accordionItem close') {
-            this.parentNode.className = 'accordionItem open';  //버블링...해야하는딩...
-        }
-    }
-
-//menu
-
 
 })();
